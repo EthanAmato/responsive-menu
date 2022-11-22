d0d8f1edee9263567bcd5b7dbf0a09bc291bb1af
EthanAmato <ethannamato@gmail.com>
2022-11-22 10:52:24 -0500

    removed console.log() from scripts

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ const menu = document.getElementById("menu");
 
 Array.from(document.getElementsByClassName('menu-item')) //allow menu items to be iterable
     .forEach((item, index) => {
-        console.log(item)
         item.onmouseover = () => {
             menu.dataset.activeIndex = index; //update an active index to the currently
                                             //hovered index for proper transitions
